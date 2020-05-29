d13f77aac87664f7866b9c99e9b001f2c1cba5cb
Benjamin Levy <blevy@protonmail.com>
2020-05-28 20:06:06 -0400

    Disable blinking text on BIOSes that make it default

--- mine.asm
+++ mine.asm
@@ -53,6 +53,24 @@ BootMine:
   xor ax, ax
   int 0x10
 
+  ; Disable blinking text
+  ; https://www.reddit.com/r/osdev/comments/70fcig/blinking_text/dn2t6u8?utm_source=share&utm_medium=web2x
+  ; Read I/O Address 0x03DA to reset index/data flip-flop
+  mov dx, 0x03DA
+  in al, dx
+  ; Write index 0x30 to 0x03C0 to set register index to 0x30
+  mov dx, 0x03C0
+  mov al, 0x30
+  out dx, al
+  ; Read from 0x03C1 to get register contents
+  inc dx
+  in al, dx
+  ; Unset Bit 3 to disable Blink
+  and al, 0xF7
+  ; Write to 0x03C0 to update register with changed value
+  dec dx
+  out dx, al
+
   ; Load VGA text buffer segment into es
   mov dx, TextBuf.Seg
   mov es, dx
